25ebcfde6526594744a69bfd0debc1c3c9b078ae
Salman Hassan <manujutt1307@gmail.com>
2024-08-08 15:00:18 +0500

    add JS

---
+++ dasso.js
@@ -0,0 +1,12 @@
+console.log(`Hello`);
+console.log(`I like Pizza`);
+
+window.alert(`This is an alert`);
+window.alert(`I like pasta`);
+
+document.getElementById("H1").textContent = 'Hello';
+document.getElementById("P").textContent=`I like Pizza`;
+
+// This is a single line comment
+
+/* This is a double line comment*/
