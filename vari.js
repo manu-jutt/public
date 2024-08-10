e5d3a44928d5fb140c7b0861caf65202b147d5a0
Salman Hassan <manujutt1307@gmail.com>
2024-08-10 15:05:34 +0500

    variable

---
+++ vari.js
@@ -0,0 +1,7 @@
+let fullname = "Salman Hassan";
+let age = 20;
+let isstudent = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p2").textContent = `Your age is ${age}`;
+document.getElementById("p3").textContent = `You are a student: ${isstudent}`;
