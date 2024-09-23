03d27d002fe6daa748d3607efcb3c312bd1d5e6c
Salman Hassan <manujutt1307@gmail.com>
2024-09-23 21:30:25 +0500

    yes

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Radius:</label><br>
+        <input type="number" name="radius"><br>
+        <input type="submit" value="Calculate">    
+    </form>
+</body>
+</html>
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+    $radius = $_POST["radius"];
+    $circumference = null;
+    $area = null;
+    $volume = null;
+
+    $circumference = 2 * pi() * $radius;
+    $circumference = round($circumference, 2);
+
+    $area = pi() * pow($radius, 2);
+    $area = round($area, 2);
+
+    $volume = 4/3 * pi() * pow($radius, 3);
+    $volume = round($volume, 2);
+
+    echo"Circumference = {$circumference} <br>";
+    echo"Area = {$area}<br>";
+    echo"Volume = {$volume}<br>";
 ?>
-Something is wrong with the XAMPP installation :-(
